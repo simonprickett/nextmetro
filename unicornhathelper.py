611d97ca2aa2b6874150bd257a25675b4a517dd8
root <root@pi20150127.(none)>
2015-02-04 15:15:25 -0500

    Rough working version

--- unicornhathelper.py
+++ unicornhathelper.py
@@ -9,6 +9,7 @@
 #####
 
 import unicornhat as UH
+import time
 
 #####
 # TODO descriptions
@@ -50,7 +51,24 @@ _testTransform = [
 	[ [1, 8], [2, 8], [3, 8], [4, 8], [5, 8], [6, 8], [7, 8], [8, 8] ]
 ]
 
-
+_testTransform2 = [
+	[ [1, 1], [2, 1], [3, 1], [4, 1] ], 
+	[ [5, 1], [6, 1], [7, 1], [8, 1] ],
+	[ [1, 2], [2, 2], [3, 2], [4, 2] ],
+        [ [5, 2], [6, 2], [7, 2], [8, 2] ],
+	[ [1, 3], [2, 3], [3, 3], [4, 3] ], 
+	[ [5, 3], [6, 3], [7, 3], [8, 3] ],
+	[ [1, 4], [2, 4], [3, 4], [4, 4] ],
+	[ [5, 4], [6, 4], [7, 4], [8, 4] ],
+	[ [1, 5], [2, 5], [3, 5], [4, 5] ], 
+	[ [5, 5], [6, 5], [7, 5], [8, 5] ],
+	[ [1, 6], [2, 6], [3, 6], [4, 6] ], 
+	[ [5, 6], [6, 6], [7, 6], [8, 6] ],
+	[ [1, 7], [2, 7], [3, 7], [4, 7] ], 
+	[ [5, 7], [6, 7], [7, 7], [8, 7] ],
+	[ [1, 8], [2, 8], [3, 8], [4, 8] ], 
+	[ [5, 8], [6, 8], [7, 8], [8, 8] ]
+]
 #####
 # TODO description
 #####
@@ -74,7 +92,7 @@ def getTransformNames():
 #####
 # TODO description
 #####
-def generateSingleColourMatrix(r, g, b):
+def generateSingleColorMatrix(r, g, b):
 	if (r < 0 or r > 255):
 		raise ValueError('r must be between 0 and 255')
 		return
@@ -98,11 +116,30 @@ def applyTransform(ledMatrix, tranformName, paintDelay):
 
 	# Check transformName is valid
 	#TODO
+	transformMatrix = _testTransform2
 
 	# Check paint delay is valid
 	if (paintDelay < 0):
 		raise ValueError('paintDelay must be 0 or greater')
 		return
+
+	print ledMatrix
+	for nextStepLEDs in transformMatrix:
+		print nextStepLEDs
+		for individualLED in nextStepLEDs:
+			x = individualLED[0] - 1
+			y = individualLED[1] - 1
+			print "x = " + str(x) + ", y = " + str(y)
+			
+			colorsToPaint = ledMatrix[x][y]
+			r = colorsToPaint[0]
+			g = colorsToPaint[1]
+			b = colorsToPaint[2]
+			UH.set_pixel(x, y, r, g, b)
+			print "set to r = " + str(r) + ", g = " + str(g) + ", b = " + str(b)
+
+		UH.show()
+		time.sleep(paintDelay)
 
 #####
 # TODO description
