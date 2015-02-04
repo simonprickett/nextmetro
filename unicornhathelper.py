7cd89b01d2a1ad9d5172135fbe32538b530bfa5f
Simon Prickett <simon@crudworks.org>
2015-02-04 17:41:31 -0500

    Added a diagonal fade transition

--- unicornhathelper.py
+++ unicornhathelper.py
@@ -175,6 +175,25 @@ _testTransform5 = [
 	[ [8, 8] ]
 ]
 
+# radiate from bottom left corner out
+_testTransform6 = [
+	[ [1, 8] ],
+	[ [1, 7], [2, 8] ],
+	[ [1, 6], [2, 7], [3, 8] ],
+	[ [1, 5], [2, 6], [3, 7], [4, 8] ],
+	[ [1, 4], [2, 5], [3, 6], [4, 7], [5, 8] ],
+	[ [1, 3], [2, 4], [3, 5], [4, 6], [5, 7], [6, 8] ],
+	[ [1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7], [7, 8] ],
+	[ [1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7], [8, 8] ],
+	[ [2, 1], [3, 2], [4, 3], [5, 4], [6, 5], [7, 6], [8, 7] ],
+	[ [3, 1], [4, 2], [5, 3], [6, 4], [7, 5], [8, 6] ],
+	[ [4, 1], [5, 2], [6, 3], [7, 4], [8, 5] ],
+	[ [5, 1], [6, 2], [7, 3], [8, 4] ],
+	[ [6, 1], [7, 2], [8, 3] ],
+	[ [7, 1], [8, 2] ],
+	[ [8, 1] ]
+]
+
 #####
 # TODO description
 #####
@@ -218,7 +237,8 @@ def applyTransform(ledMatrix, transformName, paintDelay):
 		transformMatrix = _testTransform4
 	elif (transformName == "test5"):
 		transformMatrix = _testTransform5
-
+	elif (transformName == "test6"):
+		transformMatrix = _testTransform6
 
 	# Check paint delay is valid
 	if (paintDelay < 0):
